20e1b32cfccf2f22aa52f950fdd238709aea2a51
sai <saidevopsworld@gmail.com>
2022-11-20 14:50:48 +0000

    copied from sampleapp

---
+++ src/main/java/com/xyz/sampleapp/HelloWorld.java
@@ -0,0 +1,18 @@
+package works.template.samples;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorld extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
+        PrintWriter writer = response.getWriter();
+        writer.print("Hello Brainstroming demo.");
+        writer.close();
+    }
+}
